dfc0737179151a0f6d70c01a7ac354f00d79d151
chrsolr <chrsolr@pop-os.localdomain>
2023-08-09 13:14:19 -0500

    2022 Day 03 Part-2 Completed

--- src/2022/day-03.ts
+++ src/2022/day-03.ts
@@ -48,6 +48,30 @@ export async function day3_PartTwo_2022() {
   console.info('---------------------------------------------------')
   console.info('Advent of Code: Day 3 of 2022 (Part #2)')
   console.info(`Instruction @: ${INSTRUCTIONS_LINK}`)
+
+  // group every 3 lines
+  // find letter that is in each line
+  // letter = score for the group
+  // sum all groups
+
+  const exampleInput = `
+    vJrwpWtwJgWrhcsFMMfFFhFp
+    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+    PmmdzqPrVvPwwTWBwg
+    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+    ttgJtRGJQctTZtZT
+    CrZsJsPPZsGzwwsLwLmpwMDw`
+    .split('\n')
+    .filter((v) => v)
+    .map((v) => v.trim())
+
+  const filepath = path.join(__dirname, 'files/2022_day_3_input.txt')
+  const lines = (await readFileLineByLine(filepath))
+    .filter((v) => v)
+    .map((v) => v.trim())
+
+  console.info('Total Example Score:', calculateScoreForPartTwo(exampleInput))
+  console.info('Total Answer Score:', calculateScoreForPartTwo(lines))
 }
 
 function calculateScoreForPartOne(lines: string[]) {
@@ -73,5 +97,33 @@ function calculateScoreForPartOne(lines: string[]) {
       }
     }
   }
+  return total
+}
+
+function calculateScoreForPartTwo(lines: string[]) {
+  const groupByLengthOf = 3
+  let groupHolder = []
+  let total = 0
+
+  for (const line of lines) {
+    groupHolder.push(line)
+
+    if (groupHolder.length === groupByLengthOf) {
+      const [first, second, thrid] = groupHolder
+
+      const matches = first.split('').map((letter) => ({
+        letter,
+        matched: second.includes(letter) && thrid.includes(letter),
+      }))
+
+      const [badge] = Array.from(
+        new Set(matches.filter((v) => v.matched).map((v) => v.letter)),
+      )
+      const [, score] = alphabet.find((v) => v[0] === badge) || ['', 0]
+      total += score
+      groupHolder = []
+    }
+  }
+
   return total
 }
