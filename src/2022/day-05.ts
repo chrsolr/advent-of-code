254998218f0e755290b894fd46f277808e842b60
chrsolr <chrsolr@pop-os.localdomain>
2023-08-20 17:57:55 -0500

    2022 Day 05 Part-1 WIP

--- src/2022/day-05.ts
+++ src/2022/day-05.ts
@@ -13,26 +13,35 @@ export async function day5_PartOne_2022() {
   console.info(`Instruction @: ${INSTRUCTIONS_LINK}`)
 
   const exampleInput = `
-        [D]    
-    [N] [C]    
-    [Z] [M] [P]
-    1   2   3 
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
 
-    move 1 from 2 to 1
-    move 3 from 1 to 3
-    move 2 from 2 to 1
-    move 1 from 1 to 2`
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`
     .split('\n')
     .filter((v) => v)
-    .map((v) => v.trim())
 
   const filepath = path.join(__dirname, 'files/2022_day_5_input.txt')
-  const lines = (await readFileLineByLine(filepath))
-    .filter((v) => v)
-    .map((v) => v.trim())
+  const lines = (await readFileLineByLine(filepath)).filter((v) => v)
 
-  console.info('Total Example Score:')
-  console.info('Total Answer Score:')
+  const { stack: stackExample, operations: operationsExample } =
+    getStackAndOperations(exampleInput)
+  const { stack, operations } = getStackAndOperations(lines)
+  console.info(
+    'Total Example Score:',
+    solvePartOne(stackExample, operationsExample),
+  )
+  console.info(
+    'Total Answer Score:',
+    solvePartOne(
+      stack.filter((v) => v),
+      operations,
+    ),
+  )
 }
 
 export async function day5_PartTwo_2022() {
@@ -61,4 +70,80 @@ export async function day5_PartTwo_2022() {
 
   console.info('Total Example Score:')
   console.info('Total Answer Score:')
+}
+
+function getStackAndOperations(arr: string[]) {
+  const { stack, moves } = arr.reduce(
+    (memo, current) => {
+      console.log(current)
+      if (current.includes('move')) {
+        memo.moves.push(current)
+        return memo
+      }
+
+      if (current.includes('1')) {
+        return memo
+      }
+
+      const lineChars = current.split('')
+
+      for (let i = 0; i < lineChars.length; i++) {
+        const value = lineChars[i]
+
+        if (value && value !== ' ') {
+          const cargo = `${lineChars[i]}${lineChars[i + 1]}${lineChars[i + 2]}`
+          const stackIndex = Math.floor((i + 3) / 3 - 1)
+
+          if (!memo.stack[stackIndex]) {
+            memo.stack[stackIndex] = []
+          }
+
+          memo.stack[stackIndex].unshift(cargo)
+
+          i += 2
+        }
+      }
+
+      return memo
+    },
+    { stack: [], moves: [] } as {
+      stack: string[][]
+      moves: string[]
+    },
+  )
+
+  const operations = moves.reduce((memo, current) => {
+    const [amount, from, to] = current
+      .split(' ')
+      .filter((v) => !isNaN(Number(v)))
+      .map((v) => +v)
+
+    memo.push({ amount, from, to })
+    return memo
+  }, [] as { amount: number; from: number; to: number }[])
+
+  return { stack, operations }
+}
+
+function solvePartOne(
+  stack: string[][],
+  operations: { amount: number; from: number; to: number }[],
+) {
+  // this is mutating the stack array.
+  // maybe us reduce do make it more readable
+  for (let i = 0; i < operations.length; i++) {
+    const { amount, from, to } = operations[i]
+
+    const itemsToMove = stack[from - 1].splice(stack[from - 1].length - amount)
+    stack[to - 1].push(...itemsToMove.reverse())
+  }
+
+  const result = stack
+    .filter((v) => v && v.length)
+    .map(([v]) => `${v}`)
+    .join('')
+    .replace(/\[/g, '')
+    .replace(/\]/g, '')
+
+  return result
 }
