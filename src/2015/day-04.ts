e56eb6386d5fee27c78e6ccf1932709e24d755ee
chrsolr <chr.solr@gmail.com>
2023-11-11 00:09:42 -0600

    clean up day 4 - 2015

--- src/2015/day-04.ts
+++ src/2015/day-04.ts
@@ -1,47 +1,30 @@
 import * as crypto from 'crypto'
+import { printResult } from '../shared/utils'
 
 /**
  * Day 04
  * Instructions: https://adventofcode.com/2015/day/4
  */
-const INSTRUCTIONS_LINK = 'https://adventofcode.com/2015/day/4'
 
-function printResult(count: number, part: number): void {
-  console.info('***************************************************')
-  console.info(`Advent of Code: Day 4 of 2015 (Part #${part})`)
-  console.info(`Instruction @: ${INSTRUCTIONS_LINK}`)
-  console.info('Total Answer Score:', count)
-}
+const calculateMD5Hash = (input: string): string =>
+  crypto.createHash('md5').update(input).digest('hex')
 
-function calculateMD5Hash(input: string): string {
-  const md5Hash = crypto.createHash('md5')
-  md5Hash.update(input)
-  return md5Hash.digest('hex')
-}
-
-export function solve(secret: string, searchTerm: string): number {
+const solve = (secret: string, searchTerm: string): number => {
+  let matched = false
   let count = 0
 
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
-    const key = `${secret}${count}`
-    const md5 = calculateMD5Hash(key)
-    const matched = md5.startsWith(searchTerm)
-
-    if (matched) {
-      return count
-    }
-
+  while (!matched) {
     count++
+    matched = calculateMD5Hash(`${secret}${count}`).startsWith(searchTerm)
   }
+
+  return count
 }
 
-export async function day4_PartOne_2015() {
-  const answer = solve('ckczppom', '00000')
-  printResult(answer, 1)
-}
+export default () => {
+  const answerPartOne = solve('ckczppom', '00000')
+  const answerPartTwo = solve('ckczppom', '000000')
 
-export async function day4_PartTwo_2015() {
-  const answer = solve('ckczppom', '000000')
-  printResult(answer, 2)
+  printResult(answerPartOne, 2015, 4, 1)
+  printResult(answerPartTwo, 2015, 4, 2)
 }
--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,63 @@
 import _2015_01 from './2015/day-01'
 import _2015_02 from './2015/day-02'
 import _2015_03 from './2015/day-03'
+import _2015_04 from './2015/day-04'
 import { day1_2022 } from './2022/day-01'
 import { day2_PartOne_2022, day2_PartTwo_2022 } from './2022/day-02'
 import { day3_PartOne_2022, day3_PartTwo_2022 } from './2022/day-03'
 import { day4_PartOne_2022, day4_PartTwo_2022 } from './2022/day-04'
 import { day5_PartOne_2022, day5_PartTwo_2022 } from './2022/day-05'
-import { day4_PartOne_2015, day4_PartTwo_2015 } from './2015/day-04'
 import { day5_PartOne_2015 } from './2015/day-05'
 import { day6_PartOne_2015 } from './2015/day-06'
 
 const [, , challenge] = process.argv
 
 ;(async () => {
-  try {
-    if (!challenge || challenge === '2015-01') {
-      _2015_01()
-    }
+  const map = new Map<string, Function>()
+    .set('2015-01', _2015_01)
+    .set('2015-02', _2015_02)
+    .set('2015-03', _2015_03)
+    .set('2015-04', _2015_04)
 
-    if (!challenge || challenge === '2015-02') {
-      _2015_02()
-    }
-
-    if (!challenge || challenge === '2015-03') {
-      _2015_03()
-    }
-
-    if (!challenge || challenge === '2015-04') {
-      await day4_PartOne_2015()
-      await day4_PartTwo_2015()
-    }
-
-    if (!challenge || challenge === '2015-05') {
-      day5_PartOne_2015()
-    }
-
-    if (!challenge || challenge === '2015-06') {
-      day6_PartOne_2015()
-    }
-
-    if (!challenge || challenge === '2022-01') {
-      await day1_2022()
-    }
-
-    if (!challenge || challenge === '2022-02') {
-      await day2_PartOne_2022()
-      await day2_PartTwo_2022()
-    }
-
-    if (!challenge || challenge === '2022-03') {
-      await day3_PartOne_2022()
-      await day3_PartTwo_2022()
-    }
-
-    if (!challenge || challenge === '2022-04') {
-      await day4_PartOne_2022()
-      await day4_PartTwo_2022()
-    }
-
-    if (!challenge || challenge === '2022-05') {
-      await day5_PartOne_2022()
-      await day5_PartTwo_2022()
-    }
-    console.info('***************************************************')
-  } catch (error) {
-    console.error('ERROR:', error)
+  if (!challenge) {
+    // TODO: run all
+    return
   }
+
+  if (map.has(challenge)) {
+    map.get(challenge)!()
+  }
+
+  if (!challenge || challenge === '2015-05') {
+    day5_PartOne_2015()
+  }
+
+  if (!challenge || challenge === '2015-06') {
+    day6_PartOne_2015()
+  }
+
+  if (!challenge || challenge === '2022-01') {
+    await day1_2022()
+  }
+
+  if (!challenge || challenge === '2022-02') {
+    await day2_PartOne_2022()
+    await day2_PartTwo_2022()
+  }
+
+  if (!challenge || challenge === '2022-03') {
+    await day3_PartOne_2022()
+    await day3_PartTwo_2022()
+  }
+
+  if (!challenge || challenge === '2022-04') {
+    await day4_PartOne_2022()
+    await day4_PartTwo_2022()
+  }
+
+  if (!challenge || challenge === '2022-05') {
+    await day5_PartOne_2022()
+    await day5_PartTwo_2022()
+  }
+  console.info('***************************************************')
 })()
