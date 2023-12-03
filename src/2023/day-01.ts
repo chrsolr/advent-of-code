e78469b733e3bfac2fc7a627b526948951b4fa95
chrsolr <chr.solr@gmail.com>
2023-12-03 13:35:35 -0600

    2023-01 changed from recursive to bottom-up

--- src/2023/day-01.ts
+++ src/2023/day-01.ts
@@ -15,31 +15,30 @@ const keywords = [
   'nine',
 ]
 
-const recursive = (
-  previousChar: string,
-  chars: string[] = [],
-  memo: string[] = [],
-): string[] => {
-  if (chars.length === 0) {
-    return memo
+const calculatePartTwo = (line: string[]): string[] => {
+  const memo: string[] = []
+  let previousChar = ''
+
+  while (line.length > 0) {
+    const currentChar = line.shift() ?? ''
+
+    if (!isNaN(Number(currentChar))) {
+      memo.push(currentChar)
+    } else {
+      const combinedChars = `${previousChar}${currentChar}`
+      const keywordIndex = keywords.findIndex((key) =>
+        combinedChars.includes(key),
+      )
+
+      if (keywordIndex > -1) {
+        memo.push((keywordIndex + 1).toString())
+      }
+
+      previousChar = keywordIndex > -1 ? currentChar : combinedChars
+    }
   }
 
-  const currentChar = chars.shift() ?? ''
-
-  if (!isNaN(Number(currentChar))) {
-    memo.push(currentChar)
-    return recursive(currentChar, chars, memo)
-  }
-
-  const combinedChars = `${previousChar}${currentChar}`
-  const keywordIndex = keywords.findIndex((key) => combinedChars.includes(key))
-
-  if (keywordIndex > -1) {
-    memo.push((keywordIndex + 1).toString())
-    return recursive(currentChar, chars, memo)
-  }
-
-  return recursive(combinedChars, chars, memo)
+  return memo
 }
 
 const partOneReducer = (memo: number, line: string) => {
@@ -58,7 +57,7 @@ const partOneReducer = (memo: number, line: string) => {
 }
 
 const partTwoReducer = (memo: number, line: string) => {
-  const numbers = recursive('', line.split(''))
+  const numbers = calculatePartTwo(line.split(''))
 
   const firstNumber = numbers[0]
   const lastNumber = numbers[numbers.length - 1]
