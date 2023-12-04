01a77682fcc65b1b390fb37b2bde8bc15584abbe
chrsolr <chr.solr@gmail.com>
2023-12-03 23:44:11 -0600

    2023-02 solved part 1 but ugly

--- src/2023/day-02.ts
+++ src/2023/day-02.ts
@@ -1,11 +1,48 @@
-import input from './files/input-day-02'
+import input, { example } from './files/input-day-02'
 import { printResult } from '../shared/utils'
 
-const getInputData = (): string[] => input.split('\n')
+type Color = 'red' | 'green' | 'blue'
 
-const solvePartOne = (lines: string[]) => {
-  return lines.length
+const getInputData = (): string[] => (example || input).split('\n')
+
+const maxCube = new Map<Color, number>([
+  ['red', 12],
+  ['green', 13],
+  ['blue', 14],
+])
+
+// Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+const partOneReducer = (line: string) => {
+  const captures = line.match(/(Game [0-9]+: )|([^;]*)/g) || []
+  const [, ...sets] = captures.filter((v) => v).map((v) => v.trim())
+
+  for (const set of sets) {
+    const cubes = set.split(', ')
+    let isValid = true
+
+    for (const cube of cubes) {
+      const [num, color] = cube.split(' ') as Color[]
+      const max = maxCube.get(color) ?? 0
+
+      if (Number(num) > max) {
+        isValid = false
+        break
+      }
+    }
+
+    if (!isValid) {
+      return false
+    }
+  }
+
+  return true
 }
+
+const solvePartOne = (lines: string[]) =>
+  lines.reduce(
+    (memo, line, index) => (partOneReducer(line) ? memo + index + 1 : memo),
+    0,
+  )
 
 const solvePartTwo = (lines: string[]) => {
   return lines.length
