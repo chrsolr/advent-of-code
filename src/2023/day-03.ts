d62ef1cd10e473f71c147065a3dedbb987108006
chrsolr <chr.solr@gmail.com>
2024-05-15 21:32:25 -0500

    Added 2023-03 part 1

--- src/2023/day-03.ts
+++ src/2023/day-03.ts
@@ -1,55 +1,105 @@
-import input, { example } from './files/input-day-03'
+import input from './files/input-day-03'
 import { printResult } from '../shared/utils'
 
-const getInputData = (): string[] =>
-  (example || input)
-    .split('\n')
-    .filter((line) => line)
-    .slice(0, 2)
+type MapItem = {
+  digit: string
+  coordinates: string[]
+  beenAdded: boolean
+}
 
+const getInputData = (): string[] => input.split('\n').filter((line) => line)
 const isNumber = (character: string) => !isNaN(+character)
 
-const createMap = (lines: string[]) => {
-  const map = []
+const createMap = (lines: string[]): MapItem[] => {
+  let y = 0
 
-  for (const line of lines) {
-    let digits = ''
-    let coords = []
+  return lines.flatMap((line) => {
+    const items: MapItem[] = []
 
     let x = 0
-    let y = 0
+    let digit = ''
+    let coords: string[] = []
 
     for (const char of line) {
-      if (isNumber(char) && isNumber(line[x + 1])) {
-        digits += char
-        coords.push(`${x},${y}`)
-      } else if (isNumber(char) && !isNumber(line[x + 1])) {
-        digits += char
+      if (isNumber(char)) {
+        digit += char
         coords.push(`${x},${y}`)
 
-        map.push({
-          digits,
-          coords,
-        })
-      } else {
-        coords = []
-        digits = ''
+        if (!isNumber(line[x + 1])) {
+          items.push({
+            digit,
+            coordinates: coords,
+            beenAdded: false,
+          })
+
+          digit = ''
+          coords = []
+        }
       }
+
       x++
     }
 
     y++
+
+    return items
+  })
+}
+
+const getSurroundingCoordinates = (x: number, y: number): string[] => {
+  const directions = [-1, 0, 1]
+  const neighboards = []
+
+  for (const dx of directions) {
+    for (const dy of directions) {
+      if (dx !== 0 || dy !== 0) {
+        neighboards.push(`${x + dx},${y + dy}`)
+      }
+    }
   }
+
+  return neighboards
 }
 
 const solvePartOne = (lines: string[]) => {
-  const map = createMap(lines)
+  const mapItems = createMap(lines)
 
-  return lines.length
+  let y = 0
+  let x = 0
+
+  let total = 0
+
+  for (const line of lines) {
+    for (const char of line) {
+      if (isNumber(char) || char === '.') {
+        x++
+        continue
+      }
+
+      const potentialCoords = getSurroundingCoordinates(x, y)
+
+      for (const c of potentialCoords) {
+        const ff = mapItems.findIndex((v) => v.coordinates.includes(c))
+        const a = mapItems[ff]
+
+        if (a !== undefined && !a.beenAdded) {
+          total += Number(a.digit)
+          mapItems[ff].beenAdded = true
+        }
+      }
+
+      x++
+    }
+
+    y++
+    x = 0
+  }
+
+  return total
 }
 
 const solvePartTwo = (lines: string[]) => {
-  return lines.length
+  return 0
 }
 
 export default () => {
