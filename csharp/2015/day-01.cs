a1ceb1b124669352f393f24f52d60bdd70473ff4
chrsolr <chr.solr@gmail.com>
2025-01-13 19:21:02 -0600

    Setup Program.cs

---
+++ csharp/2015/day-01.cs
@@ -0,0 +1,11 @@
+internal class _2015_Day01
+{
+    public _2015_Day01() { }
+
+    public static string title = "2015, Day 01";
+
+    public void run()
+    {
+        Console.WriteLine("2015, Day 01");
+    }
+}
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,17 +1,30 @@
-﻿if (args.Length > 0)
+﻿// See https://aka.ms/new-console-template for more information
+
+var map = new Dictionary<string, Action>
 {
-    foreach (var arg in args)
-    {
-        Console.WriteLine($"Argument: {arg}");
-    }
-}
-else
+    { "2015-01", () => new _2015_Day01().run() },
+};
+
+if (args.Length <= 0)
 {
     Console.WriteLine(
         "No arguments provided. Please provide a year and day as arguments in the format of 'YYYY-DD'."
     );
+
+    return 0;
 }
 
-Console.WriteLine("Hello, World!");
+string date = args[0];
+
+if (map.ContainsKey(date) == false)
+{
+    Console.WriteLine($"Invalid date argument: {date}");
+
+    return 0;
+}
+
+Console.WriteLine($"Date argument: {date}");
+
+Console.WriteLine("***************************************************");
 
 return 0;
