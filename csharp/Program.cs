22bd01e737ed1c1cdd324ffe59f9a97e571301ac
chrsolr <chr.solr@gmail.com>
2025-01-13 18:45:33 -0600

    Added CSharp

---
+++ csharp/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+if (args.Length > 0)
+{
+    foreach (var arg in args)
+    {
+        Console.WriteLine($"Argument={arg}");
+    }
+}
+else
+{
+    Console.WriteLine("No arguments");
+}
+
+await Task.Delay(5000);
+
+Console.WriteLine("Hello, World!");
+
+return 0;
