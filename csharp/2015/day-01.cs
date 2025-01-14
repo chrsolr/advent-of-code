c2ab379bb36e52f6756eee775a8cdfc0df6b6235
chrsolr <chr.solr@gmail.com>
2025-01-13 20:39:49 -0600

    Finished 2015-01 C#

--- csharp/2015/day-01.cs
+++ csharp/2015/day-01.cs
@@ -4,24 +4,49 @@
  */
 internal class _2015_Day01
 {
-    public static int SolvePartOne(List<string> inputData)
+    private int calculateFloor(List<char> chars) =>
+        chars.Aggregate(0, (memo, current) => memo += current == '(' ? 1 : -1);
+
+    private int calculatePosition(List<char> chars, int total = 0, int position = 0)
     {
-        return inputData.Count;
+        if (chars.Count == 0 || total == -1)
+        {
+            return position;
+        }
+
+        char c = chars.FirstOrDefault();
+
+        int currentTotal = total + (c == '(' ? 1 : -1);
+
+        return calculatePosition(chars.Skip(1).ToList(), currentTotal, position + 1);
     }
 
-    public static int SolvePartTwo(List<string> inputData)
+    private int SolvePartOne(List<char> inputData)
     {
-        return inputData.Count;
+        return calculateFloor(inputData);
+    }
+
+    private int SolvePartTwo(List<char> inputData)
+    {
+        return calculatePosition(inputData);
     }
 
     public void runAsync()
     {
         const string inputFileDate = "2015-01";
 
-        var inputData = FileUtils.GetInputData(inputFileDate);
+        var inputData = FileUtils.GetInputData(inputFileDate).FirstOrDefault();
 
-        var answerPartOne = SolvePartOne(inputData);
-        var answerPartTwo = SolvePartTwo(inputData);
+        if (inputData == null)
+        {
+            Console.WriteLine("No input data found for this day");
+            return;
+        }
+
+        List<char> data = inputData.Trim().ToList();
+
+        var answerPartOne = SolvePartOne(data);
+        var answerPartTwo = SolvePartTwo(data);
 
         Utils.PrintResult(answerPartOne, 2015, 1, 1);
         Utils.PrintResult(answerPartTwo, 2015, 1, 2);
